42b901f3c431ca0161f64a7ccd012140ae902681
abnrk <adudowas@gmail.com>
2026-02-14 12:58:23 +0000

    Update makehosts.sh

--- makehosts.sh
+++ makehosts.sh
@@ -1,3 +1,7 @@
 curl -O https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+echo "0.0.0.0 cdnbo.org" >> hosts
+echo "0.0.0.0 us-cdnbo.org" >> hosts
+echo "0.0.0.0 hakurei.us-cdnbo.org" >> hosts
 echo "0.0.0.0 creatives.us-cdnbo.org" >> hosts
 echo "0.0.0.0 hakurei.cdnbo.org" >> hosts
+echo "0.0.0.0 creatives.cdnbo.org" >> hosts
